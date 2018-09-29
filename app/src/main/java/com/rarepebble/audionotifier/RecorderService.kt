2bc82a752f861ddee4555438331ebe0202ff1945
Martin Stone <martin-stone@users.noreply.github.com>
2018-09-29 19:39:44 +0100

    Renames, tweaks.

--- app/src/main/java/com/rarepebble/audionotifier/RecorderService.kt
+++ app/src/main/java/com/rarepebble/audionotifier/RecorderService.kt
@@ -7,11 +7,10 @@ import android.media.AudioFormat
 import android.media.AudioRecord
 import android.media.MediaRecorder
 import android.os.IBinder
-import java.io.File
 
 private const val sampleRate = 44100
-private const val chunkSeconds = 1
-private const val chunkFrames = sampleRate * chunkSeconds
+private const val bufferSeconds = 1
+private const val bufferSamples = sampleRate * bufferSeconds
 
 class RecorderService : Service() {
 
@@ -38,9 +37,9 @@ private fun startRecording() {
     val bytesPerFrame = 2
     val encoding = AudioFormat.ENCODING_PCM_16BIT
     val minBuffBytes = AudioRecord.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_IN_MONO, encoding)
-    if (chunkFrames < minBuffBytes / bytesPerFrame) throw UnsupportedOperationException("chunk too small")
+    if (bufferSamples < minBuffBytes / bytesPerFrame) throw UnsupportedOperationException("chunk too small")
 
-    val buffBytes = chunkFrames * bytesPerFrame
+    val buffBytes = bufferSamples * bytesPerFrame
 
     val recorder = AudioRecord(
             MediaRecorder.AudioSource.MIC,
@@ -50,26 +49,24 @@ private fun startRecording() {
             buffBytes
     )
 
-    recorder.setPositionNotificationPeriod(chunkFrames)
+    recorder.setPositionNotificationPeriod(bufferSamples)
     recorder.setRecordPositionUpdateListener(listener)
     recorder.startRecording()
 }
 
 private val listener = object : AudioRecord.OnRecordPositionUpdateListener {
-    private val buffer = ShortArray(chunkFrames)
-    private val byteBuffer = ByteArray(chunkFrames*2)
-    private val file = File("/sdcard/Download/audio.raw")
+    private val buffer = ShortArray(bufferSamples)
+    //private val byteBuffer = ByteArray(bufferSamples*2)
+    //private val file = File("/sdcard/Download/audio.raw")
 
     override fun onMarkerReached(recorder: AudioRecord?) {}
 
     override fun onPeriodicNotification(recorder: AudioRecord) {
-//        val numRead = recorder.read(buffer, 0, chunkFrames)
-//        val rms = Math.sqrt(
-//                buffer.map { (it * it).toDouble() }.sum()
-//        )
-        val numRead = recorder.read(byteBuffer, 0, chunkFrames*2)
-        file.appendBytes(byteBuffer)
-        log("period: numRead $numRead")
+        val numRead = recorder.read(buffer, 0, bufferSamples)
+        val rms = Math.sqrt(buffer.sumByDouble{(it * it).toDouble()})
+//        val numRead = recorder.read(byteBuffer, 0, bufferSamples*2)
+//        file.appendBytes(byteBuffer)
+        log("period: numRead $numRead, $rms")
     }
 
 }
