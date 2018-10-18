f9fb158f737f1daf4b0f34135f63c963e615c4fc
Martin Stone <martin-stone@users.noreply.github.com>
2018-10-18 13:13:14 +0100

    Added wake lock

--- app/src/main/java/com/rarepebble/audionotifier/DetectorService.kt
+++ app/src/main/java/com/rarepebble/audionotifier/DetectorService.kt
@@ -2,10 +2,14 @@ package com.rarepebble.audionotifier
 
 import android.app.Notification
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
+import android.os.PowerManager
 
 class DetectorService : Service() {
+
+    private lateinit var wakeLock: PowerManager.WakeLock
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
@@ -14,12 +18,24 @@ class DetectorService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         startForeground(1, Notification())
+        wakeLock = (getSystemService(Context.POWER_SERVICE) as PowerManager).run {
+            newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "AndroidAudioNotifier").apply {
+                acquire()
+            }
+        }
+
 
         val detector = AlarmDetector(this::onDetected)
         startDetectingFrequencies(detector::onFrequency)
 
         return super.onStartCommand(intent, flags, startId)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        wakeLock.release()
+    }
+
 
     private fun onDetected() {
     }
